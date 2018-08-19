8d4a3498beb7d290e1a4e18117264ed8fd5ee03a
Lauren Chavez <laurenchavez@Laurens-MacBook-Pro.local>
2018-08-18 18:25:23 -0700

    adding everything

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,13 @@
 //Update the name of the controller below and rename the file.
-const template = require("../controllers/template.js")
+const cards = require("../controllers/cards.js")
 module.exports = function(app){
 
-  app.get('/', template.index);
+  app.get('/', cards.index);
+
+  app.get('/cards/add/:id', cards.add);
+
+  app.post('/cards', cards.create);
+
+  app.get('/cards/remove/:id', cards.remove);
 
 }
